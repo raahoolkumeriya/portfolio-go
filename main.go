37e1c78d5fc48647a063351f41539d2e2d3cd888
raahoolkumeriya <kumeriya_rahul@rediffmail.com>
2024-07-08 22:41:05 +0530

    new

--- main.go
+++ main.go
@@ -6,11 +6,27 @@ import (
 )
 
 func helloWorldPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "Hello World")
+	case "/universe":
+		fmt.Fprint(w, "Hello Universe ")
+	default:
+		fmt.Fprint(w, "<h1>Nowhere!</h1>")
+	}
+	fmt.Printf("What type of request comming %s\n", r.Method)
+}
+
+func plaintext(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprint(w, "<h1>Nowhere!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", helloWorldPage)
+	//http.HandleFunc("/", plaintext)
+
 	http.ListenAndServe("", nil)
 
 }
